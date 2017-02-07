cc839940a00c468b2b76ed0eec8c372800a80700
afkza4aem <nastypinagrably@gmail.com>
2017-02-07 20:04:59 +0300

    add cart_get_number_of_items function

--- public/app.js
+++ public/app.js
@@ -12,8 +12,26 @@
 function add_to_cart(id)
 {
 	var key = 'product_' + id
-	
+
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
+}
+
+function cart_get_number_of_items()
+{
+	var cnt = 0;
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); // get key
+		var value = window.localStorage.getItem(key); // get value, same as in Ruby: hh[key] = x
+
+		if(key.indexOf('product_') == 0)
+		{
+			cnt = cnt + value * 1;
+		}
+	}
+
+	return cnt;
 }
